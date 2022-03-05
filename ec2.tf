5145561cc6fdae4c1f1572f7778582a5d6e86f76
rajavasista <vasistatatapudi@gmail.com>
2022-03-05 11:46:32 +0000

    Added terraform snippet

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,20 @@
-module "myec2_calling_module" {
+# module "myec2_calling_module" {
     
-    source = "../terraform-modules/ec2/module"
+#     source = "git::ssh://git@github.com:rajavasista/terraform-modules.git//ec2/module?ref=main"
     
-    instance_count = var.instance_count
+#     instance_count = var.instance_count
 
-    ami_id = data.aws_ami.ubuntu.id
-    # ami_id = var.ami_id
+#     ami_id = data.aws_ami.ubuntu.id
+#     # ami_id = var.ami_id
 
-    instance_type = var.instance_type
+#     instance_type = var.instance_type
 
-    project = "MY_TF_PRO"
+#     project = "MY_TF_PRO"
 
-    key_name = module.my_keypair_calling_module.key_name
+#     key_name = module.my_keypair_calling_module.key_name
 
-    public_subnet_id = module.my_public_subnet_calling_module.my_public_subnet_id
+#     public_subnet_id = module.my_public_subnet_calling_module.my_public_subnet_id
 
-    vpc_security_group_ids = module.my_sg_calling_module.security_group_id
+#     vpc_security_group_ids = module.my_sg_calling_module.security_group_id
 
-}
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
-module "myvpc_calling_module" {
+# module "myvpc_calling_module" {
 
-    # Local Repo Reference
-    # source = "../terraform-modules/vpc/module"
-    # GITHUB Repo Reference
-    source = "git::ssh://git@github.com/rajavasista/terraform-modules.git//vpc/module?ref=main"
+#     # Local Repo Reference
+#     # source = "../terraform-modules/vpc/module"
+#     # GITHUB Repo Reference
+#     source = "git::ssh://git@github.com/rajavasista/terraform-modules.git//vpc/module?ref=main"
 
-    vpc_cidr_block = var.vpc_cidr_block
-    project = "INFRA_TRAINING"
+#     vpc_cidr_block = var.vpc_cidr_block
+#     project = "INFRA_TRAINING"
 
-}
+# }
