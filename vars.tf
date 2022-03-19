0ae5139e9897aea2339a411074034f0d4a259baa
rajavasista <vasistatatapudi@gmail.com>
2022-03-19 12:00:16 +0000

    added

--- vars.tf
+++ vars.tf
@@ -1,66 +1,66 @@
-# variable "region" {
-#   type = string
-#   default = "eu-west-2"
-# }
+variable "region" {
+  type = string
+  default = "eu-west-2"
+}
 
-# variable "vpc_cidr_block" {
-#   type = string
-#   default = "10.0.0.0/25"
-# }
+variable "vpc_cidr_block" {
+  type = string
+  default = "10.0.0.0/25"
+}
 
-# # Private Subnet Variables
-# variable "public_subnet_cidr_block_a" {
-#   type = string
-#   default = "10.0.0.0/28"
-# }
+# Private Subnet Variables
+variable "public_subnet_cidr_block_a" {
+  type = string
+  default = "10.0.0.0/28"
+}
 
-# variable "public_subnet_cidr_block_b" {
-#   type = string
-#   default = "10.0.0.16/28"
-# }
+variable "public_subnet_cidr_block_b" {
+  type = string
+  default = "10.0.0.16/28"
+}
 
-# variable "public_subnet_cidr_block_c" {
-#   type = string
-#   default = "10.0.0.32/28"
-# }
+variable "public_subnet_cidr_block_c" {
+  type = string
+  default = "10.0.0.32/28"
+}
 
-# # Private Subnet Variables
-# variable "private_subnet_cidr_block_a" {
-#   type = string
-#   default = "10.0.0.64/28"
-# }
+# Private Subnet Variables
+variable "private_subnet_cidr_block_a" {
+  type = string
+  default = "10.0.0.64/28"
+}
 
-# variable "private_subnet_cidr_block_b" {
-#   type = string
-#   default = "10.0.0.64/28"
-# }
+variable "private_subnet_cidr_block_b" {
+  type = string
+  default = "10.0.0.64/28"
+}
 
-# variable "private_subnet_cidr_block_c" {
-#   type = string
-#   default = "10.0.0.64/28"
-# }
+variable "private_subnet_cidr_block_c" {
+  type = string
+  default = "10.0.0.64/28"
+}
 
-# variable "instance_count" {
-#   type = number
-#   default = 1
-# }
+variable "instance_count" {
+  type = number
+  default = 1
+}
 
-# variable "instance_type" {
-#   type = string
-#   default = "t2.micro"
-# }
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
 
-# variable "public_subnets" {
-#   type = list(string)
-#   description = "list of public subnets"
-#   default = [ "10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28" ]
-# }
+variable "public_subnets" {
+  type = list(string)
+  description = "list of public subnets"
+  default = [ "10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28" ]
+}
 
-# variable "private_subnets" {
-#   type = list(string)
-#   description = "list of private subnets"
-#   default = [ "10.0.0.64/28", "10.0.0.80/28", "10.0.0.96/28" ]
-# }
+variable "private_subnets" {
+  type = list(string)
+  description = "list of private subnets"
+  default = [ "10.0.0.64/28", "10.0.0.80/28", "10.0.0.96/28" ]
+}
 
 # # 10.0.0.0/25 = 128 IP's : VPC CIDR Block - (eu-west-2) - London - 3 AZ's (2a,2b,2c)
 
